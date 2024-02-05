7396d9371eea0acefb698c0975943c911fe0df8b
どりー_Hack'z <friedrich_hackz@dori-HackznoMacBook-Pro.local>
2024-02-05 23:24:34 +0900

    clean update

--- functions/src/triggers/https/slack-bot/registers/commands/test.ts
+++ functions/src/triggers/https/slack-bot/registers/commands/test.ts
@@ -1,15 +1,23 @@
 import { logger } from 'firebase-functions'
 import { App } from '@slack/bolt'
+import { NGROK_URL } from '~/utils/secret'
+
+import fetch from 'node-fetch'
 
 export const registerTestCommand = (app: App) => {
-  app.command('/test', async ({ ack, body, context }) => {
+  app.command('/keyopen', async ({ ack, body, context }) => {
     await ack()
+
+    const response = await fetch(NGROK_URL)
+    const pageBody = await response.text()
+
+    logger.log(pageBody)
 
     try {
       await app.client.chat.postMessage({
         token: context.botToken,
         channel: body.channel_id,
-        text: 'test'
+        text: '解錠!'
       })
     } catch (err) {
       logger.log(err)
--- functions/src/utils/secret.ts
+++ functions/src/utils/secret.ts
@@ -2,3 +2,5 @@
 
 export const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET
 export const SLACK_ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN
+
+export const NGROK_URL = process.env.NGROK_URL ?? ""
