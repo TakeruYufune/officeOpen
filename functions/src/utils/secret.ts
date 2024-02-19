f9324f93b5108104cde10dd4a98a30ef6f44ede8
どりー_Hack'z <friedrich_hackz@dori-HackznoMacBook-Pro.local>
2024-02-20 04:23:24 +0900

    clean commit

--- functions/src/utils/secret.ts
+++ functions/src/utils/secret.ts
@@ -3,4 +3,4 @@
 export const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET
 export const SLACK_ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN
 
-export const NGROK_URL = process.env.NGROK_URL ?? ""
+export const NGROK_URL = process.env.NGROK_URL ?? ''
